da8dc4e2dbf6f0176c4f2661e70db3dd5267ab42
jaireflectiontp <jai.reflectiontp@gmail.com>
2023-06-21 15:51:54 +0530

    HomePage Created
    Signed-off-by: jaireflectiontp <jai.reflectiontp@gmail.com>

--- xz-shop/src/components/Homepage.js
+++ xz-shop/src/components/Homepage.js
@@ -1,14 +1,49 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
-
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 const Homepage = () => {
   return (
-    <div>
-      Homepage
-      <Link to="/">Dhahs</Link>
-      <Link to="/cart">fgdfgf</Link>
-      <Outlet />
-    </div>
+    <>
+      <nav>
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand as={Link} to="/">
+              React-Bootstrap
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Navbar.Text>
+                  <h4 style={{ margin: "0 10px 0 30px" }}>1880-188-801</h4>
+                </Navbar.Text>
+              </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+            <Navbar.Collapse className="justify-content-end">
+              <Navbar.Text>
+                Signed in as: <a href="#login">Mark Otto</a>
+              </Navbar.Text>
+              <Nav.Link as={Link} to="/cart">
+                Cart
+              </Nav.Link>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Outlet />
+      </nav>
+    </>
   );
 };
 
