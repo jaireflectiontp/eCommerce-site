9f603ad8e34285997aef3752375b8ce5b78e9d6a
jai.reflectiontp <jai.reflectiontp@gmail.com>
2023-07-18 12:00:48 +0530

    routes updated

---
+++ xz-shop/src/components/RootLayout/RootLayout.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { Provider } from "react-redux";
+import store from "../../services/store";
+import { Outlet } from "react-router-dom";
+import NavigationBar from "../../components/Navbar/Navbar";
+const RootLayout = () => {
+    return (
+        <>
+            <Provider store={store}>
+                <NavigationBar />
+                <main>
+                    <Outlet />
+                </main>
+            </Provider>
+        </>
+    );
+};
+
+export default RootLayout;
+
