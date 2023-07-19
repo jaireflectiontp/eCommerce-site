69635aea1b4aedfb8891336c442a043f59ae4346
jai.reflectiontp <jai.reflectiontp@gmail.com>
2023-07-19 14:55:41 +0530

    product loading state management implemented

--- xz-shop/src/services/store.js
+++ xz-shop/src/services/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./slices/cartSlice";
 //import formSlice from "./slices/formSlice";
 import authSlice from "./slices/authSlice";
+import productSlice from "./slices/productSlice";
 
 const store = configureStore({
   reducer: {
     cart: cartSlice,
-   // form: formSlice,
-    auth: authSlice
+    // form: formSlice,
+    auth: authSlice,
+    products: productSlice
   },
 });
 
