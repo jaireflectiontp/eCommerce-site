69adc151a4acd8861c63bdf07945e4225d7b1f2f
jai.reflectiontp <jai.reflectiontp@gmail.com>
2023-07-20 17:04:14 +0530

    created backend api for product detail managed using useReducer

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,19 @@ const app = express();
 
 app.get('/api/products/', (req, res) => {
     res.send(data.products)
-})
+});
 
-const port = process.env.PORT || 2552
+app.get('/api/products/title/:title', (req, res) => {
+    const product = data.products.find(x => x.title === req.params.title)
+    if (product) {
+        res.send(product)
+    }
+    else {
+        res.status(404).send({ message: 'never got product' })
+    }
+});
+
+const port = process.env.PORT || 2552;
 
 app.listen(port, () => {
     console.log(`serve at http://localhost:${port}`)
--- xz-shop/src/pages/ProductDetail/ProductDetailsPage.js
+++ xz-shop/src/pages/ProductDetail/ProductDetailsPage.js
@@ -1,13 +1,60 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useReducer, useState } from 'react';
 import { useParams } from 'react-router-dom'
 
 const ProductDetailsPage = () => {
     const params = useParams();
     const { title } = params
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case 'FETCH_REQ':
+                return { ...state, loading: true }
+
+            case 'FETCH_SUCCESS':
+                return { ...state, product: action.payload, loading: false }
+
+            case 'FETCH_FAIL':
+                return { ...state, loading: false, error: action.payload }
+
+            default:
+                return state
+        }
+    }
+    const [{ loading, error, product }, dispatch] = useReducer(reducer, {
+        loading: true,
+        error: '',
+        product: []
+    })
+
+    const fetchData = async () => {
+        dispatch({ type: 'FETCH_REQ' })
+        try {
+            const response = await axios.get(`/api/products/title/${title}`);
+            dispatch({ type: 'FETCH_SUCCESS', payload: response.data })
+            console.log('myproduct', response.data)
+
+        } catch (err) {
+            dispatch({ type: 'FETCH_FAIL', payload: err.message })
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, [title]);
+
     return (
-        <div>
-            <h1>{title}</h1>
-        </div>
+        loading ?
+            (<h2>Loading.....</h2>)
+            : error ?
+                (<h2>{error}</h2>)
+                : (
+                    <div>
+                        <h1>{product.title}</h1>
+                        <div>
+                            <img src={product.image} alt="" style={{ width: '400px', height: '400px' }} />
+                        </div>
+                    </div>
+                )
     )
 }
 
