e9134116c5373bd9f66fe158b9631aa48870ae80
jai.reflectiontp <jai.reflectiontp@gmail.com>
2023-07-20 17:01:59 +0530

    product slice removed

--- xz-shop/src/services/store.js
+++ xz-shop/src/services/store.js
@@ -2,14 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./slices/cartSlice";
 //import formSlice from "./slices/formSlice";
 import authSlice from "./slices/authSlice";
-import productSlice from "./slices/productSlice";
 
 const store = configureStore({
   reducer: {
     cart: cartSlice,
     // form: formSlice,
-    auth: authSlice,
-    products: productSlice
+    auth: authSlice
   },
 });
 
