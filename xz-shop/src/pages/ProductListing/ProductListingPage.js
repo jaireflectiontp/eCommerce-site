9d84b668e0778a8e0b4252abc0f691858f9cec93
jai.reflectiontp <jai.reflectiontp@gmail.com>
2023-07-20 17:01:32 +0530

    implemented state management on data fetching

--- xz-shop/src/pages/ProductListing/ProductListingPage.js
+++ xz-shop/src/pages/ProductListing/ProductListingPage.js
@@ -1,50 +1,54 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
 import { useEffect } from "react";
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { addToCart } from "../../services/slices/cartSlice";
-import { fetchProductsAsync, selectError, selectLoading, selectProducts } from "../../services/slices/productSlice";
 import ProductCard from "../../components/ProductCard/ProductCard";
+import axios from "axios";
 
 const ProductListingPage = () => {
+    const dispat = useDispatch()
 
-    const dispatch = useDispatch();
-    const products = useSelector(selectProducts);
-    const loading = useSelector(selectLoading);
-    const error = useSelector(selectError);
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case 'FETCH_REQ':
+                return { ...state, loading: true }
+
+            case 'FETCH_SUCCESS':
+                return { ...state, products: action.payload, loading: false }
+
+            case 'FETCH_FAIL':
+                return { ...state, loading: false, error: action.payload }
+
+            default:
+                return state
+        }
+    }
+    const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+        loading: true,
+        error: '',
+        products: []
+    })
+
+    const fetchData = async () => {
+        dispatch({ type: 'FETCH_REQ' })
+        try {
+            const response = await axios.get("/api/products");
+            dispatch({ type: 'FETCH_SUCCESS', payload: response.data })
+
+        } catch (err) {
+            dispatch({ type: 'FETCH_FAIL', payload: err.message })
+        }
+    };
 
     useEffect(() => {
-        dispatch(fetchProductsAsync());
-    }, [dispatch]);
-
-    if (loading) {
-        return <div>Loading...</div>;
-    }
-
-    if (error) {
-        return <div>Error: {error}</div>;
-    }
-
-    /* const [products, setProducts] = useState([]);
-     const fetchData = async () => {
-         try {
-             const response = await axios.get("/api/products");
-             setProducts(response.data);
- 
-         } catch (err) {
-             console.log("Error", err);
-         }
-     };
- 
-     useEffect(() => {
-         fetchData();
-     }, []);*/
-
+        fetchData();
+    }, []);
 
     console.log('product', products);
     const addProduct = (product) => {
-        dispatch(addToCart(product));
+        dispat(addToCart(product));
     };
 
     const productCard = products.map((product) => (
@@ -55,6 +59,10 @@ const ProductListingPage = () => {
 
     return (
         <div>
+            {
+                loading ? <h2>Loading....</h2>
+                    :
+                    <h2>{error}</h2>}
             <h1>Product Dashboard</h1>
             <Row>
                 <div className="products" style={{ padding: '15px', display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>{productCard}</div>
