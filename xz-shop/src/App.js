1baea5f15b37e214a5f64a390e4d456d9fba0814
jai.reflectiontp <jai.reflectiontp@gmail.com>
2023-10-18 22:38:31 +0530

    checkout template added

--- xz-shop/src/App.js
+++ xz-shop/src/App.js
@@ -14,16 +14,28 @@ import ContactPage from "./pages/Contact/ContactPage";
 import RootLayout from "./components/RootLayout/RootLayout";
 import HomePage from "./pages/Home/HomePage";
 import ProductDetailsPage from "./pages/ProductDetail/ProductDetailsPage";
+//import ProductListingPage from "./pages/ProductListing/ProductListingPage";
+import AccountPage from "./pages/Account/AccountPage";
+import Products from "./pages/ProductsListing/ProductsListing";
+import Profile from "./components/Profile/Profile";
+import Address from "./components/Address/Address";
+import Checkout from "./pages/Checkout/Checkout";
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<RootLayout />}>
         <Route index element={<HomePage />}></Route>
-        <Route path="/cart" element={<CartPage />}></Route>
+        <Route path="/cart" element={<CartPage />}> </Route>
+        <Route path="/checkout" element={<Checkout />}></Route>
+        <Route path="/products" element={<Products />}></Route>
         <Route path="/product/:slug" element={<ProductDetailsPage />}></Route>
         <Route path="/contact" element={<ContactPage />}></Route>
         <Route path="/signup" element={< RegistrationForm />}></Route>
         <Route path="/signin" element={<LoginForm />}></Route>
+        <Route path="account" element={<AccountPage />}>
+          <Route path="profile" element={<Profile />}></Route>
+          <Route path="address" element={<Address />}></Route>
+        </Route>
       </Route>
     )
   );
--- xz-shop/src/pages/Cart/CartPage.js
+++ xz-shop/src/pages/Cart/CartPage.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux/es/hooks/useSelector";
 import { useDispatch } from "react-redux";
-import { addToCart, decreaseCart, removeFromCart } from "../../services/slices/cartSlice";
+import { addToCart, decreaseCart, getCartTotal, removeFromCart } from "../../services/slices/cartSlice";
 import emptyCartImg from "../../assets/images/empty-cart.png"
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "../../assets/styles/index.scss"
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 const CartPage = () => {
     const cartProduct = useSelector((state) => state.cart.cartProducts);
-
-    console.log('cart', cartProduct);
+    const cartTotal = useSelector((state) => state.cart);
     const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getCartTotal());
+    }, [cartTotal, dispatch])
     const removeProduct = (product) => {
         dispatch(removeFromCart(product));
     };
@@ -23,13 +25,14 @@ const CartPage = () => {
     const removeQuantity = (product) => {
         dispatch(decreaseCart(product))
     }
+    console.log(cartTotal.cartTotalotalQuantity, cartTotal.cartTotalotalAmount);
 
     const productCard = cartProduct.map((product) => (
         <div className="col-md-3" style={{ marginBottom: "20px" }}>
             <div className="cart-product-wrapper">
                 <div className="cart-col">
-                    <img src={product.image} alt={product.slug} style={{ width: "150px", height: "180px" }} /></div>
-                <div className="cart-col">{product.slug}</div>
+                    <Link to={`/product/${product.slug}`}> <img src={product.image} alt={product.slug} style={{ width: "150px", height: "180px" }} /></Link></div>
+                <div className="cart-col text-start"><Link className="text-decoration-none" to={`/product/${product.slug}`}>{product.slug}</Link></div>
                 <div className="cart-col design-type"><span className="product-tags">{product.clothingType}</span> <span className="product-tags">{product.design}</span></div>
                 <div className="cart-col">
                     <div className="quantity-control">
@@ -48,18 +51,28 @@ const CartPage = () => {
         </div>
     ));
     return (
-        <div>
+        <div className="cart-container">
             {
-                productCard.length == 0 &&
-                <div style={{ display: 'grid', placeItems: 'center' }}>
-                    <div className="empty-cart-display">
-                        <img src={emptyCartImg} alt="empty cart" width={'400px'} />
-                        <h4 className="mb-4">Your cart is empty</h4>
-                        <button className="empty-cart-btn"><Link to='/products' >Continue Shopping</Link></button>
+                productCard.length !== 0 ?
+                    <div className="d-flex justify-content-between">
+                        <div className="cart-lists">
+                            {productCard}
+                        </div>
+                        <div className="cartSubTotal">
+                            <p><span>Cart Subtotal:</span><span>&#8377; {cartTotal.cartTotalotalAmount}</span></p>
+                            <div><Link to='/checkout'>Checkout</Link></div>
+                            <button><Link to='/products'>Continue shopping</Link></button>
+                        </div>
                     </div>
-                </div>
+                    :
+                    <div className="d-grid align-items-center justify-content-center">
+                        <div className="empty-cart-display">
+                            <img src={emptyCartImg} alt="empty cart" width={'400px'} />
+                            <h4 className="mb-4">Your cart is empty</h4>
+                            <button className="empty-cart-btn"><Link to='/products' >Continue Shopping</Link></button>
+                        </div>
+                    </div>
             }
-            {productCard}
 
         </div>
     );
