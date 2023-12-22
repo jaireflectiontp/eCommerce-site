17d077b3243473cf2b3f18a30229cccf190c0cf5
jai.reflectiontp <jai.reflectiontp@gmail.com>
2023-12-23 00:11:37 +0530

    utilities updated

---
+++ xz-shop/src/utils/sortUtils.js
@@ -0,0 +1,37 @@
+
+export const sortProducts = (products, sortType) => {
+    const sortedProducts = [...products];
+    if (sortType === 'asc') {
+        sortedProducts.sort((a, b) => a.price - b.price);
+    } else if (sortType === 'desc') {
+        sortedProducts.sort((a, b) => b.price - a.price);
+    }
+    return sortedProducts;
+};
+
+export const applyFilters = (products, filters) => {
+    let filteredProducts = [...products];
+
+    if (filters.priceRange.min !== '' && filters.priceRange.max !== '') {
+        filteredProducts = filteredProducts.filter(
+            (product) =>
+                product.price >= parseInt(filters.priceRange.min) &&
+                product.price <= parseInt(filters.priceRange.max)
+        );
+    }
+
+    if (filters.clothingType !== '') {
+        filteredProducts = filteredProducts.filter(
+            (product) => product.clothingType === filters.clothingType
+        );
+    }
+
+    if (filters.category !== '') {
+        filteredProducts = filteredProducts.filter(
+            (product) => product.category === filters.category
+        );
+    }
+
+    return filteredProducts;
+};
+
