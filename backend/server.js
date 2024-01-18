be53a91c820ac1407c1198087e9e53a5e742cbf7
jai.reflectiontp <jai.reflectiontp@gmail.com>
2024-01-18 11:45:30 +0530

    server recreated

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,41 @@
-import express from 'express';
-import { data } from './data.js';
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
 const app = express();
+const port = process.env.PORT || 2552;
+const productsData = JSON.parse(fs.readFileSync(path.join(__dirname, "productsData.json")));
+
 app.get('/api/products/', (req, res) => {
-    res.send(data.products)
+    res.status(200).json({
+        status: "success",
+        productsData
+    })
 });
 
 app.get('/api/products/slug/:slug', (req, res) => {
-    const product = data.products.find(x => x.slug === req.params.slug)
+    const product = productsData.products.find(item => item.slug === req.params.slug)
     if (product) {
         res.send(product)
     }
     else {
-        res.status(404).send({ message: 'never got product' })
+        res.status(404).send({ message: 'Product Not Found' })
     }
 });
+
 app.get('/api/products/:query', (req, res) => {
     const searchQuery = req.params.query;
-    const searchResults = data.products.filter(product =>
+    const searchResults = productsData.products.filter(product =>
         product.name.toLowerCase().includes(searchQuery)
     );
-
+    console.log(searchResults);
     if (searchResults.length > 0) {
         res.send(searchResults);
     } else {
-        res.status(404).send({ message: 'No products found matching the search query' });
+        res.status(404).send({ message: 'No Product Forund In Search' });
     }
 });
-const port = process.env.PORT || 2552;
 
 app.listen(port, () => {
-    console.log(`serve at http://localhost:${port}`)
-})
+    console.log(`Server is runing at http://localhost:${port}`)
+});
