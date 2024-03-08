2b044dd7dac6883171bbf954075785f6d782512f
jai.reflectiontp <jai.reflectiontp@gmail.com>
2024-03-08 13:27:52 +0530

    Cart sidebar layout created

--- client/src/layouts/Header.jsx
+++ client/src/layouts/Header.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react"
 import { flag, logo } from "../assets/assets"
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faCartShopping, faHeart, faMagnifyingGlass, faUser, faXmark } from '@fortawesome/free-solid-svg-icons'
+import { CartItem } from "../components"
+import '../App.css'
 const Header = () => {
     const [showSearchTab, setShowSearchTab] = useState(false)
     const [isOpen, setIsOpen] = useState(false)
+    const [isCartOpen, setIsCartOpen] = useState(false)
 
     return (
         <>
@@ -17,17 +22,17 @@ const Header = () => {
                             onChange={() => setShowSearchTab(true)}
                             onBlur={() => setShowSearchTab(false)}
                         />
-                        <button className="absolute right-0  bg-primary w-14 h-11 rounded-r text-white">df</button>
+                        <button className="absolute right-0  bg-primary w-14 h-11 rounded-r text-white"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
                         <div className={`searchResults absolute w-full h-full ${showSearchTab ? 'block' : 'hidden'}`}>
                             <div style={{ width: `calc(100% - ${56}px)` }} className="bg-slate-500 p-4">sdsd</div>
                         </div>
                     </form>
                     <div className="flex items-center justify-between w-full lg:w-fit xl:w-fit">
-                        <button onClick={() => setIsOpen(true)} className="lg:hidden">X</button>
+                        <button onClick={() => setIsOpen(true)} className="lg:hidden"><FontAwesomeIcon icon={faBars} /></button>
                         <div className="flex items-center ">
                             <ul>
                                 <li className="accountTab relative">
-                                    <a href="#" className="py-3 ps-2 pe-8">Account</a>
+                                    <span className="py-3 sm:ps-2 ps-8 sm:pe-8 pe-0"><FontAwesomeIcon icon={faUser} /><span className="hidden sm:inline pl-2">Account</span></span>
                                     <ul style={{ display: 'none' }} className="absolute w-28 top-8 left-auto rounded-md bg-slate-400 p-4 text-xs font-light">
                                         <li><Link to='/signup'>Sign up</Link></li>
                                         <li className="py-3"><Link to='/signin'>Login</Link></li>
@@ -35,16 +40,16 @@ const Header = () => {
                                     </ul>
                                 </li>
                             </ul>
-                            <a href="#" className="py-3 ps-2 pe-8">WishList</a>
-                            <a href="#" className="py-3 ps-2 pe-8">Cart</a>
+                            <button type="button" className="py-3 sm:ps-2 ps-8 sm:pe-8 pe-0"><FontAwesomeIcon icon={faHeart} /><span className="hidden sm:inline pl-2">Wishlist</span></button>
+                            <button type="button" className="py-3 sm:ps-2 ps-8 sm:pe-8 pe-0" onClick={() => setIsCartOpen(true)}><FontAwesomeIcon icon={faCartShopping} /><span className="hidden sm:inline pl-2">Cart</span></button>
                         </div>
                     </div>
                 </div>
 
                 <div className="flex justify-between items-center py-4 text-sm">
                     <div className={`overLay bg-black opacity-80 w-full h-full fixed top-0 left-0 z-10 block ${isOpen ? 'block' : 'hidden'}`}></div>
-                    <nav className={`header-navigation sm:bg-transparent bg-primary absolute sm:static top-0 left-0 w-3/4 sm:w-auto h-full sm:h-auto z-20 ${isOpen ? 'block' : 'hidden'}`}>
-                        <div className="sm:hidden flex justify-between items-center py-8 px-6 border-b-[1px]"><span>Menu</span> <button onClick={() => setIsOpen(!isOpen)}>X</button></div>
+                    <nav className={`header-navigation sm:bg-transparent bg-primary absolute sm:static top-0 left-0 w-3/4 sm:w-auto h-full sm:h-auto z-20 ${isOpen ? 'block' : 'hidden sm:block'}`}>
+                        <div className="sm:hidden flex justify-between items-center py-8 px-6 border-b-[1px]"><span>Menu</span> <button onClick={() => setIsOpen(!isOpen)}><FontAwesomeIcon icon={faXmark} /></button></div>
                         <ul className="flex gap-1 sm:gap-6 sm:flex-row flex-col p-8 sm:p-0">
                             <li className="border-b-[1px] sm:border-b-0"><a href="#" className="px-2 py-3 inline-block sm:inline">Home</a></li>
                             <li className="border-b-[1px] sm:border-b-0"><a href="#" className="px-2 py-3 inline-block sm:inline">Products</a></li>
@@ -55,6 +60,42 @@ const Header = () => {
                         </ul>
                     </nav>
                     <div className="hidden sm:flex gap-1"><img src={flag} alt="" className="w-5" />+91 8822000454</div>
+                </div>
+            </div>
+
+
+            <div className={`fixed top-0 right-0 h-full z-20 bg-slate-200 ${isCartOpen ? 'block' : 'hidden'}`}>
+                <div className="px-5 w-[300px] sm:w-[350px] h-full flex flex-col gap-4">
+                    <div className="h-3/4">
+                        <div className="flex justify-between py-4 mb-4 border-b-[1px] border-gray-400">
+                            <h6>My Cart</h6>
+                            <button className="px-4" onClick={() => setIsCartOpen(false)}><FontAwesomeIcon icon={faXmark} /></button>
+                        </div>
+                        <ul className="cartItem_list overflow-y-scroll h-[85%]">
+                            <CartItem />
+                            <CartItem />
+                            <CartItem />
+                            <CartItem />
+                            <CartItem />
+                            <CartItem />
+                        </ul>
+                    </div>
+                    <div className="h-1/4 border-t-[1px] border-gray-400 px-3">
+                        <div className="mt-5 mb-7 pb-2">
+                            <div className="mt-2 flex justify-between items-center text-lg">
+                                <strong>Sub Total:</strong>
+                                <strong>256525</strong>
+                            </div>
+                        </div>
+                        <div className="flex justify-between">
+                            <button type="button" className="inline-flex items-center rounded-md bg-primary px-5 py-[10px] text-sm font-semibold text-white shadow-sm hover:bg-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                View Cart
+                            </button>
+                            <button type="button" className="inline-flex items-center rounded-md bg-transparent px-5 py-[10px] text-sm font-semibold border-[1px] border-primary hover:border-black text-primary hover:text-white shadow-sm hover:bg-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                Checkout
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
